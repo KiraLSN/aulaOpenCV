940f5fa343073557f352952b59a972d5476ebd54
luciano <lucsoaam@gmail.com>
2019-11-19 18:30:10 -0400

    Incompleto

--- houghCircle.py
+++ houghCircle.py
@@ -1,13 +1,12 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('dire.jpg')
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-edges = cv2.Canny(gray, 35, 200, apertureSize=3)
-minLineLenght = 100
-maxLineGap = 20
-lines = cv2.HoughLinesP(edges,1,np.pi/100,100,minLineLenght,maxLineGap)
-for x1, y1, x2, y2 in lines[0]:
-    cv2.line(img,(x1,x2),(x2,y2), (0,255,0),2)
-    cv2.imwrite('bingo.jpg',img)
-    k = cv2.waitKey(10) 
+img = cv2.imread('dire.jpg',0)
+img = cv2. medianBlur(img,5)
+cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+
+circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=0,maxRadius=0)
+circles = np.uint16(np.around(circles))
+for i in circles[0,:]:
+    #draw the outer circle
+    cv2.circle(cimg,(i[0],i[1],i[2],(0,255,0)))
